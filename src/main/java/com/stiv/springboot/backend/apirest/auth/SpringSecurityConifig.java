3b2851ca80b1bb1f3484969e6d858522db104dc9
Stiv <strodriguezna@gmail.com>
2022-05-17 16:34:03 -0500

    Configuracion para servidor de autorizacion, apps CLIENTES/ACCESO/ENDPOINTS

--- src/main/java/com/stiv/springboot/backend/apirest/auth/SpringSecurityConifig.java
+++ src/main/java/com/stiv/springboot/backend/apirest/auth/SpringSecurityConifig.java
@@ -3,6 +3,7 @@ package com.stiv.springboot.backend.apirest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +24,12 @@ public class SpringSecurityConifig  extends WebSecurityConfigurerAdapter{
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean("authenticationManager")
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 	
 	
