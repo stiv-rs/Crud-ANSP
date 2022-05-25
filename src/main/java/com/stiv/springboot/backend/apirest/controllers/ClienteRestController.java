c176868c4f05b698172936b4cb5bb7a8b652d64f
Stiv <strodriguezna@gmail.com>
2022-05-24 19:14:16 -0500

    Añadiendo los PERMISOS DE USUARIOS desde @ANOTACIONES

--- src/main/java/com/stiv/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/stiv/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -58,6 +59,7 @@ public class ClienteRestController {
 		return clienteService.finAll(PageRequest.of(page, 4));
 	}
 	
+	@Secured({"ROLE_ADMIN","ROLE_USER"})
 	@GetMapping("/clientes/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id){
 		Cliente cliente = null;
@@ -79,6 +81,7 @@ public class ClienteRestController {
 		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/clientes")
 	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 		
@@ -114,6 +117,7 @@ public class ClienteRestController {
 		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED); 
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@PutMapping("/clientes/{id}")
 	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
 		
@@ -155,6 +159,7 @@ public class ClienteRestController {
 		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@DeleteMapping("/clientes/{id}")
 	public ResponseEntity<?> delete(@PathVariable Long id) {
 
@@ -177,6 +182,7 @@ public class ClienteRestController {
 	
 	}
 	
+	@Secured({"ROLE_ADMIN","ROLE_USER"})
 	@PostMapping("/clientes/upload")
 	public ResponseEntity<?> upload(@RequestParam ("archivo") MultipartFile archivo, @RequestParam("id") Long id){
 		
@@ -227,6 +233,7 @@ public class ClienteRestController {
 		return new ResponseEntity<Resource>(recurso, cabecera ,HttpStatus.OK);
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/clientes/regiones")
 	public List<Region> listarRegiones(){
 		return clienteService.findAllRegiones();
