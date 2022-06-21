495eb6a6228cba9da20b0b654dbc97deaf62a842
Stiv <strodriguezna@gmail.com>
2022-06-21 11:17:19 -0500

    Creando nueva clase ITEM FACTURA en el entity

--- src/main/java/com/stiv/springboot/backend/apirest/models/entity/Factura.java
+++ src/main/java/com/stiv/springboot/backend/apirest/models/entity/Factura.java
@@ -1,8 +1,11 @@
 package com.stiv.springboot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -35,10 +39,19 @@ public class Factura implements Serializable {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
 	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id")
+	private List<ItemFactura> items;
+	
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
 	}
+	
+	public Factura() {
+		items = new ArrayList<>();
+	}
+	
 
 	public Long getId() {
 		return id;
@@ -80,6 +93,16 @@ public class Factura implements Serializable {
 		this.cliente = cliente;
 	}
 	
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+
+
 	private static final long serialVersionUID = 1L;
 
 }
