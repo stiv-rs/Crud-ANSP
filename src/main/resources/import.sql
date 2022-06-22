36431b8efe8eceee752024ca081e3230530ab1e0
Stiv <strodriguezna@gmail.com>
2022-06-22 11:13:50 -0500

    Instertando datos de FACTURAS e ITEMS DE FACTURA

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -30,3 +30,19 @@ INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1,1);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,2);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,1);
+
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic Pantalla LCD',259990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara digital DSC-W320B',123490, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple iPod shuffle',1499990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook Z110',37990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Hewlett Packard Multifuncional F2280',70289, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi Bicicleta Aro 26',69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Mica Comoda 5 cajones',299980, NOW());
+
+INSERT INTO facturas(descripcion, observacion, cliente_id, create_at) VALUES ('Factura equipos de oficina', null, 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (1, 1, 1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (2, 1, 4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (1, 1, 5);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (1, 1, 7);
+
+INSERT INTO facturas(descripcion, observacion, cliente_id, create_at) VALUES ('Factura bicicleta', 'Alguna nota importante', 1, NOW());
